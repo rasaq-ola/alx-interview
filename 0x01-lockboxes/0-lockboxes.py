df55eed5c0fbdabe126be34cad1c74ff2f6d03b4
rasaq-ola <rasaqola196@gmail.com>
2024-08-17 02:32:15 +0100

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3defcanUnlockAll(boxes):
-    """ Determines if all boxes can be unlocked. """ifnot boxes:
-        returnFalse
-    
+"""Determines if all boxes can be unlocked.""" ifnot boxes:
+    returnFalse
+
     n = len(boxes)
     unlocked = set([0])
     stack = [0]
-    
+
     while stack:
         current_box = stack.pop()
         for key in boxes[current_box]:
             if key < n and key notin unlocked:
                 unlocked.add(key)
                 stack.append(key)
-    
+
     returnlen(unlocked) == n
