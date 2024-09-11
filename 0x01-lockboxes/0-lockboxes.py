1d8e0f95291632162c0c23f7e81f3e84ce334e4e
rasaq-ola <rasaqola196@gmail.com>
2024-09-11 20:25:43 +0100

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,17 +2,17 @@
 def canUnlockAll(boxes):
     """Determines if all boxes can be unlocked."""
     if not boxes:
-        return False
+        return True  # No boxes to unlock, return True
 
-    n = len(boxes)
-    unlocked = set([0])  # We can always unlock the first box
-    stack = [0]  # Use a stack to keep track of boxes to check
+    n = len(boxes)  # Total number of boxes
+    unlocked = set([0])  # Start with box 0 unlocked
+    stack = [0]  # Start checking from box 0
 
     while stack:
         current_box = stack.pop()
-        for key in boxes[current_box]:
-            if key < n and key not in unlocked:
+        for key in boxes[current_box]:  # Check all keys in current_box
+            if 0 <= key < n and key not in unlocked:  # Key must be valid
                 unlocked.add(key)
                 stack.append(key)
 
-    return len(unlocked) == n
+    return len(unlocked) == n  # Return True if all boxes are unlocked
